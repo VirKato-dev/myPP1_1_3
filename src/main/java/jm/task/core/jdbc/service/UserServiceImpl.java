c928aeca5559250ca8b32bf2c84281a57084e3c5
VirKato <dascat79@mail.ru>
2022-08-18 21:36:26 +0700

    Ознакомиться с заготовкой. Тесты должны пройти успешно.

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -2,9 +2,13 @@ package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.model.User;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
+    private User testUser;
+    private final List<User> testUsers = new ArrayList<>();
+
     public void createUsersTable() {
 
     }
@@ -14,7 +18,9 @@ public class UserServiceImpl implements UserService {
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        testUser = new User(name, lastName, age);
+        testUser.setId(0L);
+        testUsers.add(testUser);
     }
 
     public void removeUserById(long id) {
@@ -22,7 +28,7 @@ public class UserServiceImpl implements UserService {
     }
 
     public List<User> getAllUsers() {
-        return null;
+        return testUsers;
     }
 
     public void cleanUsersTable() {
