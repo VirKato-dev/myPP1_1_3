9cd2f63a6667e38b00e25e75c91de08328b03bea
VirKato <dascat79@mail.ru>
2022-08-21 09:46:19 +0700

    Ознакомиться с заготовкой. Тесты должны пройти успешно.

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -6,6 +6,7 @@ import jm.task.core.jdbc.service.UserServiceImpl;
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        // Hibernate
 
         UserServiceImpl tableUsers = new UserServiceImpl();
         tableUsers.createUsersTable();
