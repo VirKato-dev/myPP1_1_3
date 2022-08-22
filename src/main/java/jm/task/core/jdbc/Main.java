a87fbe08f2b624cc8388fe32f2a1c330de6c7516
VirKato <dascat79@mail.ru>
2022-08-22 12:47:37 +0700

    вариант для 1.1.3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -6,6 +6,7 @@ import jm.task.core.jdbc.service.UserServiceImpl;
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        // JDBC
         UserServiceImpl tableUsers = new UserServiceImpl();
         tableUsers.createUsersTable();
         tableUsers.saveUser("Вася", "Егоров", (byte) 20);
